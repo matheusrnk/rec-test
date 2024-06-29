4965bda4fb1e2ae13730850a15dc08f5a2d161a7
Matheus Sehnem <matheusrnk@debian-rnk-machine.matheusrnk>
2024-06-29 11:44:10 -0300

    testando 52

--- controlador.py
+++ controlador.py
@@ -1,58 +1,66 @@
-#!/usr/bin/python
-
 from ryu.base import app_manager
 from ryu.controller import ofp_event
-from ryu.controller.handler import MAIN_DISPATCHER, set_ev_cls
-from ryu.ofproto import ofproto_v1_0
-from ryu.lib.mac import haddr_to_bin
-from ryu.lib.packet import packet, ethernet, ether_types, tcp
+from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
+from ryu.controller.handler import set_ev_cls
+from ryu.ofproto import ofproto_v1_3
+from ryu.lib import dpid as dpid_lib
+from ryu.lib import stplib
+from ryu.lib.packet import packet
+from ryu.lib.packet import ethernet
+from ryu.app import simple_switch_13
 
-class L2Switch(app_manager.RyuApp):
-    OFP_VERSIONS = [ofproto_v1_0.OFP_VERSION]
+
+class SimpleSwitch13(simple_switch_13.SimpleSwitch13):
+    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+    _CONTEXTS = {'stplib': stplib.Stp}
 
     def __init__(self, *args, **kwargs):
-        super(L2Switch, self).__init__(*args, **kwargs)
+        super(SimpleSwitch13, self).__init__(*args, **kwargs)
         self.mac_to_port = {}
+        self.stp = kwargs['stplib']
 
-    def add_flow(self, datapath, in_port, dst, src, actions):
+        # Sample of stplib config.
+        #  please refer to stplib.Stp.set_config() for details.
+        config = {dpid_lib.str_to_dpid('0000000000000001'):
+                  {'bridge': {'priority': 0x8000}},
+                  dpid_lib.str_to_dpid('0000000000000002'):
+                  {'bridge': {'priority': 0x9000}},
+                  dpid_lib.str_to_dpid('0000000000000003'):
+                  {'bridge': {'priority': 0xa000}}}
+        self.stp.set_config(config)
+
+    def delete_flow(self, datapath):
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
 
-        match = parser.OFPMatch(
-            in_port=in_port, dl_dst=haddr_to_bin(dst), dl_src=haddr_to_bin(src)
-        )
+        for dst in self.mac_to_port[datapath.id].keys():
+            match = parser.OFPMatch(eth_dst=dst)
+            mod = parser.OFPFlowMod(
+                datapath, command=ofproto.OFPFC_DELETE,
+                out_port=ofproto.OFPP_ANY, out_group=ofproto.OFPG_ANY,
+                priority=1, match=match)
+            datapath.send_msg(mod)
 
-        mod = parser.OFPFlowMod(
-            datapath=datapath, match=match, cookie=0,
-            command=ofproto.OFPFC_ADD, idle_timeout=0, hard_timeout=0,
-            priority=ofproto.OFP_DEFAULT_PRIORITY,
-            flags=ofproto.OFPFF_CHECK_OVERLAP, actions=actions)
-        datapath.send_msg(mod)
-
-    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
-    def packet_in_handler(self, ev):
+    @set_ev_cls(stplib.EventPacketIn, MAIN_DISPATCHER)
+    def _packet_in_handler(self, ev):
         msg = ev.msg
         datapath = msg.datapath
-        dpid = datapath.id
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
+        in_port = msg.match['in_port']
 
         pkt = packet.Packet(msg.data)
-        pkt_eth = pkt.get_protocol(ethernet.ethernet)
+        eth = pkt.get_protocols(ethernet.ethernet)[0]
 
-        if pkt_eth.ethertype == ether_types.ETH_TYPE_LLDP:
-            return
+        dst = eth.dst
+        src = eth.src
 
-        dst = pkt_eth.dst
-        src = pkt_eth.src
-        in_port = msg.in_port
-
+        dpid = datapath.id
         self.mac_to_port.setdefault(dpid, {})
 
-        self.logger.info("packet in -> dpid: %s, src: %s, dst: %s, in_port: %s",
-                         dpid, src, dst, in_port)
+        self.logger.info("packet in %s %s %s %s", dpid, src, dst, in_port)
 
-        # Learn a mac address to avoid FLOOD next time.
+        # learn a mac address to avoid FLOOD next time.
         self.mac_to_port[dpid][src] = in_port
 
         if dst in self.mac_to_port[dpid]:
@@ -62,32 +70,37 @@ class L2Switch(app_manager.RyuApp):
 
         actions = [parser.OFPActionOutput(out_port)]
 
-        # Install a flow to avoid packet_in next time
+        # install a flow to avoid packet_in next time
         if out_port != ofproto.OFPP_FLOOD:
-            self.add_flow(datapath, in_port, dst, src, actions)
+            match = parser.OFPMatch(in_port=in_port, eth_dst=dst)
+            self.add_flow(datapath, 1, match, actions)
 
         data = None
         if msg.buffer_id == ofproto.OFP_NO_BUFFER:
             data = msg.data
 
-        out = parser.OFPPacketOut(
-            datapath=datapath, buffer_id=msg.buffer_id, in_port=in_port,
-            actions=actions, data=data)
+        out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
+                                  in_port=in_port, actions=actions, data=data)
         datapath.send_msg(out)
 
-        # Forward the packet to all other switches
-        for dp in self.mac_to_port.keys():
-            if dp != dpid:
-                dpath = self.mac_to_port[dp]
-                actions = [parser.OFPActionOutput(ofproto.OFPP_FLOOD)]
-                out = parser.OFPPacketOut(
-                    datapath=dpath, buffer_id=ofproto.OFP_NO_BUFFER, in_port=ofproto.OFPP_CONTROLLER,
-                    actions=actions, data=msg.data)
-                dpath.send_msg(out)
+    @set_ev_cls(stplib.EventTopologyChange, MAIN_DISPATCHER)
+    def _topology_change_handler(self, ev):
+        dp = ev.dp
+        dpid_str = dpid_lib.dpid_to_str(dp.id)
+        msg = 'Receive topology change event. Flush MAC table.'
+        self.logger.debug("[dpid=%s] %s", dpid_str, msg)
 
-    @set_ev_cls(ofp_event.EventOFPStateChange, MAIN_DISPATCHER)
-    def state_change_handler(self, ev):
-        datapath = ev.datapath
-        if datapath.id not in self.mac_to_port:
-            self.logger.info("New switch connected: %s", datapath.id)
-            self.mac_to_port[datapath.id] = {}
+        if dp.id in self.mac_to_port:
+            self.delete_flow(dp)
+            del self.mac_to_port[dp.id]
+
+    @set_ev_cls(stplib.EventPortStateChange, MAIN_DISPATCHER)
+    def _port_state_change_handler(self, ev):
+        dpid_str = dpid_lib.dpid_to_str(ev.dp.id)
+        of_state = {stplib.PORT_STATE_DISABLE: 'DISABLE',
+                    stplib.PORT_STATE_BLOCK: 'BLOCK',
+                    stplib.PORT_STATE_LISTEN: 'LISTEN',
+                    stplib.PORT_STATE_LEARN: 'LEARN',
+                    stplib.PORT_STATE_FORWARD: 'FORWARD'}
+        self.logger.debug("[dpid=%s][port=%d] state=%s",
+                          dpid_str, ev.port_no, of_state[ev.port_state])
---
+++ test.py
@@ -0,0 +1,67 @@
+from mininet.cli import CLI
+from mininet.net import Mininet
+from mininet.node import RemoteController
+from mininet.term import makeTerm
+
+def create_topology():
+    net = Mininet(controller=RemoteController)
+
+    # Adding controller
+    c0 = net.addController('c0', port=6633)
+
+    # Adding switches
+    switch0 = net.addSwitch('s0')
+    switch1 = net.addSwitch('s1')
+    switch2 = net.addSwitch('s2')
+    switch3 = net.addSwitch('s3')
+    switch4 = net.addSwitch('s4')
+    switch5 = net.addSwitch('s5')
+    switch6 = net.addSwitch('s6')
+
+    # Adding hosts
+    hosts = []
+    for i in range(10):
+        host = net.addHost(f'h{i}')
+        hosts.append(host)
+
+    # Adding links between switches
+    net.addLink(switch6, switch0)
+    net.addLink(switch6, switch1)
+    net.addLink(switch6, switch2)
+    net.addLink(switch0, switch3)
+    net.addLink(switch0, switch4)
+    net.addLink(switch0, switch5)
+    net.addLink(switch0, switch1)
+    net.addLink(switch0, switch2)
+
+    # Adding links between switches and hosts
+    net.addLink(switch1, hosts[0])
+    net.addLink(switch1, hosts[1])
+    net.addLink(switch3, hosts[2])
+    net.addLink(switch3, hosts[3])
+    net.addLink(switch4, hosts[4])
+    net.addLink(switch4, hosts[5])
+    net.addLink(switch5, hosts[6])
+    net.addLink(switch5, hosts[7])
+    net.addLink(switch2, hosts[8])
+    net.addLink(switch2, hosts[9])
+
+    net.build()
+    c0.start()
+    switch0.start([c0])
+    switch1.start([c0])
+    switch2.start([c0])
+    switch3.start([c0])
+    switch4.start([c0])
+    switch5.start([c0])
+    switch6.start([c0])
+
+    net.startTerms()
+
+    CLI(net)
+
+    net.stop()
+
+if __name__ == '__main__':
+    setLogLevel('info')
+    create_topology()
