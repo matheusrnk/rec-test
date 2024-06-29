cbc26d2c0632f7db1af690e0054ea6000401a0f2
Matheus Sehnem <matheusrnk@debian-rnk-machine.matheusrnk>
2024-06-29 09:13:15 -0300

    testando 49

--- test3.py
+++ test3.py
@@ -4,20 +4,20 @@ from mininet.link import TCLink
 from mininet.cli import CLI
 from mininet.log import setLogLevel
 
-def create_topology():
+def create_full_topology():
     net = Mininet(controller=RemoteController, switch=OVSKernelSwitch, link=TCLink)
 
     # Adding Ryu controller
     net.addController('c0', controller=RemoteController, ip='127.0.0.1', port=6633)
 
     # Adding switches
-    switch0 = net.addSwitch('s0', protocols='OpenFlow13', stp=True)
-    switch1 = net.addSwitch('s1', protocols='OpenFlow13', stp=True)
-    switch2 = net.addSwitch('s2', protocols='OpenFlow13', stp=True)
-    switch3 = net.addSwitch('s3', protocols='OpenFlow13', stp=True)
-    switch4 = net.addSwitch('s4', protocols='OpenFlow13', stp=True)
-    switch5 = net.addSwitch('s5', protocols='OpenFlow13', stp=True)
-    switch6 = net.addSwitch('s6', protocols='OpenFlow13', stp=True)
+    switch0 = net.addSwitch('s0', protocols='OpenFlow13')
+    switch1 = net.addSwitch('s1', protocols='OpenFlow13')
+    switch2 = net.addSwitch('s2', protocols='OpenFlow13')
+    switch3 = net.addSwitch('s3', protocols='OpenFlow13')
+    switch4 = net.addSwitch('s4', protocols='OpenFlow13')
+    switch5 = net.addSwitch('s5', protocols='OpenFlow13')
+    switch6 = net.addSwitch('s6', protocols='OpenFlow13')
 
     # Adding hosts
     hosts = []
@@ -50,6 +50,10 @@ def create_topology():
     # Start the network
     net.start()
 
+    # Enable STP
+    for switch in [switch0, switch1, switch2, switch3, switch4, switch5, switch6]:
+        switch.cmd('ovs-vsctl set Bridge %s stp_enable=true' % switch.name)
+
     # Running CLI
     CLI(net)
 
@@ -58,4 +62,4 @@ def create_topology():
 
 if __name__ == '__main__':
     setLogLevel('info')
-    create_topology()
+    create_full_topology()
