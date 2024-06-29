c7db7fb99f44f222dd527a2f8980b75b90924829
Matheus Sehnem <matheusrnk@debian-rnk-machine.matheusrnk>
2024-06-28 23:30:38 -0300

    adding testopology

---
+++ testopology.py
@@ -0,0 +1,39 @@
+from mininet.net import Mininet
+from mininet.node import RemoteController, OVSSwitch
+from mininet.cli import CLI
+from mininet.log import setLogLevel
+
+def create_topology():
+    net = Mininet(controller=RemoteController, switch=OVSSwitch)
+
+    # Add switches
+    s1 = net.addSwitch('s1')
+    s2 = net.addSwitch('s2')
+
+    # Add hosts with specific IPs
+    h1 = net.addHost('h1', ip='10.0.1.1/24')
+    h2 = net.addHost('h2', ip='10.0.2.1/24')
+
+    # Add links
+    net.addLink(h1, s1)
+    net.addLink(s1, s2)
+    net.addLink(s2, h2)
+
+    # Add Ryu controller
+    net.addController('c0', controller=RemoteController, ip='127.0.0.1', port=6653)
+
+    # Start Mininet
+    net.start()
+
+    # Start the Ryu controller
+    s1.start([c0])
+
+    # Open Mininet CLI
+    CLI(net)
+
+    # Clean up
+    net.stop()
+
+if __name__ == '__main__':
+    setLogLevel('info')
+    create_topology()
