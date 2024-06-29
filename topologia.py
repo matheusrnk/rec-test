48c609465c48a9803c76e034542ed49c7c1cdf01
Matheus Sehnem <matheusrnk@debian-rnk-machine.matheusrnk>
2024-06-29 00:42:30 -0300

    testando 5

--- topologia.py
+++ topologia.py
@@ -1,6 +1,6 @@
 from mininet.topo import Topo
 from mininet.net import Mininet
-from mininet.node import RemoteController
+from mininet.node import Controller
 from mininet.link import TCLink
 from mininet.cli import CLI
 
@@ -9,32 +9,33 @@ class CustomTopo(Topo):
         # Add switches
         s1 = self.addSwitch('s1')
         s2 = self.addSwitch('s2')
+        s3 = self.addSwitch('s3')
 
         # Add hosts
         h1 = self.addHost('h1', ip='10.0.0.1/24')
         h2 = self.addHost('h2', ip='10.0.0.2/24')
-        h3 = self.addHost('h3', ip='10.1.0.1/24')
-        h4 = self.addHost('h4', ip='10.1.0.2/24')
+        h3 = self.addHost('h3', ip='10.0.0.3/24')
+        h4 = self.addHost('h4', ip='10.0.0.4/24')
+        h5 = self.addHost('h5', ip='10.1.0.1/24')
+        h6 = self.addHost('h6', ip='10.1.0.2/24')
+        h7 = self.addHost('h7', ip='10.1.0.3/24')
 
         # Add links
         self.addLink(h1, s1)
         self.addLink(h2, s1)
+        self.addLink(h3, s2)
+        self.addLink(h4, s2)
+        self.addLink(h5, s2)
+        self.addLink(h6, s3)
+        self.addLink(h7, s3)
         self.addLink(s1, s2)
-        self.addLink(s2, h3)
-        self.addLink(s2, h4)
+        self.addLink(s2, s3)
 
 def run():
     topo = CustomTopo()
-    net = Mininet(topo=topo, controller=RemoteController, link=TCLink)
+    net = Mininet(topo=topo, controller=Controller, link=TCLink)
     net.start()
     
-    # Add routes for hosts on different subnets to communicate
-    h1, h2, h3, h4 = net.get('h1', 'h2', 'h3', 'h4')
-    h1.cmd('route add -net 10.1.0.0/24 gw 10.0.0.254')
-    h2.cmd('route add -net 10.1.0.0/24 gw 10.0.0.254')
-    h3.cmd('route add -net 10.0.0.0/24 gw 10.1.0.254')
-    h4.cmd('route add -net 10.0.0.0/24 gw 10.1.0.254')
-
     CLI(net)
     net.stop()
 
